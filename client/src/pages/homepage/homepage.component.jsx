e40f6f094640e0f71b2d66d6fe5235adc9c17bdf
Aditya Singh <samant04aditya@gmail.com>
2021-06-12 06:12:27 +0530

    backend complete

--- client/src/pages/homepage/homepage.component.jsx
+++ client/src/pages/homepage/homepage.component.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 
 import Directory from '../../components/directory/directory.component';
-
 import { HomePageContainer } from './homepage.styles';
 
 const HomePage = () => (
